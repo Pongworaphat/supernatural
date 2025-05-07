1cbeb564f467a17e20a59961eb9799bf6a171382
Pongworaphat <pongworaphat.i@ku.th>
2025-05-08 01:24:51 +0700

    commit naja

--- app/src/main/java/com/example/foodwastevanish/ui/donate/DonateViewModel.kt
+++ app/src/main/java/com/example/foodwastevanish/ui/donate/DonateViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.foodwastevanish.ui.dashboard
+package com.example.foodwastevanish.ui.donate
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class DonateViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is donate Fragment"
     }
     val text: LiveData<String> = _text
 }
