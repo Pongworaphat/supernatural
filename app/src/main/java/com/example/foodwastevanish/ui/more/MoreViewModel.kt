41c95080daadfc6880557608cb2d856322f890bc
Pongworaphat <pongworaphat.i@ku.th>
2025-05-08 02:48:56 +0700

    commit naja

--- app/src/main/java/com/example/foodwastevanish/ui/more/MoreViewModel.kt
+++ app/src/main/java/com/example/foodwastevanish/ui/more/MoreViewModel.kt
@@ -1,4 +1,20 @@
 package com.example.foodwastevanish.ui.more
 
-class MoreViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MoreViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is MoreFragment"
+    }
+    val text: LiveData<String> = _text
+
+    companion object {
+        val text: Any
+            get() {
+                TODO()
+            }
+    }
 }
